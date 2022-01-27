d865c1e0124ab84ee6c1c84e691c328b0125f4c2
Alfons Lorente Darder <alorente@cifpfbmoll.com>
2022-01-27 18:45:21 +0100

    commit 3

--- src/convolution/Convolution.java
+++ src/convolution/Convolution.java
@@ -5,17 +5,55 @@
  */
 package convolution;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 /**
  *
  * @author alfon
  */
-public class Convolution {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Convolution extends BufferedImage{
+    
+    private BufferedImage bufferedImage;
+    private int[][] pixels;
+    private int[][] kernel;
+    private int width;
+    private int height;
+    
+    
+    Convolution(BufferedImage bufferedImage){
+        this.bufferedImage = bufferedImage;
+        width = bufferedImage.getWidth();
+        height = bufferedImage.getHeight();
+        pixels = new int [width-1][height-1];
+        fillPixels();
     }
+    
+    private void sharpening(){
+        kernel = new int[][] {
+            {0, -1, 0},
+            {-1, 5, -1},
+            {0, -1, 0}
+        };
+        
+        for(int i = 1; i < width-1; i++){
+            for(int j = 1; j < height-1; j++){
+                
+            }
+        }
+        
+        
+        
+    }
+
+    private void fillPixels() {
+         for(int i = 1; i < width-1; i++){
+            for(int j = 1; j < height-1; j++){
+            }
+         }
+    }
+    
+    
+    
     
 }
--- src/convolution/Main.java
+++ src/convolution/Main.java
@@ -5,17 +5,62 @@
  */
 package convolution;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JFrame;
+
 /**
  *
  * @author alfon
  */
-public class Convolution {
+public class Main extends JFrame{
 
-    /**
-     * @param args the command line arguments
-     */
+    private GridBagConstraints constraints = new GridBagConstraints(); 
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Main();
     }
+    
+    
+    Main(){
+        setMainFrame();
+        this.setVisible(true);
+        
+        
+    }
+
+    private void setMainFrame() {
+        this.setTitle("ConvolutionTransform");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout (null);
+        this.setBounds(0, 0, 1360, 790);
+        this.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+        //this.setResizable(false);        
+        
+    }
+    
+     //setGridRules: set up the gridbag layout rules
+    private void setGridRules(){
+        //Set the constraints up for the viewer
+        constraints.gridx = 1; // El área de texto empieza en la columna cero.
+        constraints.gridy = 0; // El área de texto empieza en la fila cero
+        constraints.weightx = 1;
+        constraints.weighty = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Add the viewer with the contraints.
+        //this.add(viewer , constraints);
+        
+        //Set the constraints up for the control panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weightx = 0;
+        constraints.weighty = 1;
+        //Add the control panel with the contraints.
+        //is.add (controlPanel, constraints);
+
+    }
+    
     
 }
